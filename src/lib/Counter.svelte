cb55f3cd0445c4b6b3c1c02a3a0b99356b38973a
CalebBertrand <caleb.a.bertrand@gmail.com>
2022-10-15 23:41:37 -0500

    Theme variables set

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,12 @@
-<script>
-  let count = 0
+<script type='typescript'>
+  let count = 0;
   const increment = () => {
     count += 1
   }
+
+  $: squared = count * count;
 </script>
 
 <button on:click={increment}>
-  count is {count}
+  squared is {squared}
 </button>
