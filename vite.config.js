9d3a8df13e55c5c406d3083b9c4d44cf73d5ea69
Caleb <caleb.bertrand@verisglobal.com>
2023-01-28 18:41:25 -0600

    image minimizer

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    preprocess: preprocess({
-      postcss: true
+  plugins: [
+    svelte({
+      preprocess: preprocess({
+        postcss: true
+      })
+    }),
+    viteImagemin({
+      mozjpeg: {
+        quality: 50,
+      }
     })
-  })]
+  ]
 })
